b1c76ca4308a7ccddce5723e094e07d51e52a82d
MateusMarques28 <mateusmarques776@gmail.com>
2024-11-12 23:30:13 -0300

    Create Game Repository, service, controller and DTO

---
+++ src/main/java/com/devgersu/dslist/services/GameService.java
@@ -0,0 +1,22 @@
+package com.devgersu.dslist.services;
+
+import com.devgersu.dslist.dto.GameMinDTO;
+import com.devgersu.dslist.entities.Game;
+import com.devgersu.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+}
